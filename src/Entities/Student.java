d33ffe9540a765a6118f7e6e6da8bd8e7dbd610f
sontaypham <phamsontay2206@gmail.com>
2025-03-04 21:25:06 +0700

    first step up student management

---
+++ src/Entities/Student.java
@@ -0,0 +1,109 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name, id, belongClass, birth, faculty, phoneNumber, address;
+    public List<Student> students;
+
+    public Student(String name, String id, String belongClass, String birth, String faculty
+            , String phoneNumber, String address) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+        this.belongClass = belongClass;
+        this.faculty = faculty;
+        this.phoneNumber = phoneNumber;
+        this.birth = birth;
+        students = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBelongClass() {
+        return belongClass;
+    }
+
+    public void setBelongClass(String belongClass) {
+        this.belongClass = belongClass;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void AddStudent(Student student) {
+        students.add(student);
+    }
+
+    public void DelStudent(String _id) {
+        students.forEach(e -> {
+            if(e.equals(_id)) {
+                students.remove(e);
+                return;
+            }
+        });
+    }
+
+    public void ChangeStudent(String _id, String _name, String _belongClass, String _birth,
+                              String _faculty, String _phoneNumber, String _address) {
+        students.forEach(e -> {
+            if(e.equals(_id)) {
+                setName(_name);
+                setAddress(_address);
+                setBirth(_birth);
+                setBelongClass(_belongClass);
+                setFaculty(_faculty);
+                setPhoneNumber(_phoneNumber);
+                return;
+            }
+        });
+    }
+
+    public Student GetStudentById(String id) {
+        return;
+    }
+}
